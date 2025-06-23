56cf05809283b17c0d0a93d7d1913b0eda825e82
SpeeedWagon <gabriel.duda@deepcode.xyz>
2025-06-23 23:24:10 +0300

    commit message

--- cigniter-app/app/Controllers/BaseController.php
+++ cigniter-app/app/Controllers/BaseController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Libraries\AuthTrait; // <-- Use our new Trait
+use App\Models\AuthTokenModel;
+use App\Models\UserModel;
 use CodeIgniter\Controller;
 use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
@@ -9,50 +12,67 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
-/**
- * Class BaseController
- *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
- *
- * For security be sure to declare any new methods as protected or private.
- */
 abstract class BaseController extends Controller
 {
-    /**
-     * Instance of the main Request object.
-     *
-     * @var CLIRequest|IncomingRequest
-     */
+    use AuthTrait; // <-- This "mixes in" the methods from our trait
+
     protected $request;
+    protected $helpers = ['form', 'url']; // Good idea to load these helpers globally
+    protected $session; // A property to hold the session instance
 
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
-     *
-     * @var array
-     */
-    protected $helpers = [];
-
-    /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
-     */
-    // protected $session;
-
-    /**
-     * Constructor.
-     */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
+        // --- OUR MODIFICATIONS START HERE ---
 
-        // E.g.: $this->session = \Config\Services::session();
+        // 1. Load the session service for use in all controllers
+        $this->session = \Config\Services::session();
+
+        // 2. Attempt to automatically log in the user from the "Remember Me" cookie
+        $this->attemptLoginFromCookie();
+    }
+
+    /**
+     * Checks for a "Remember Me" cookie and logs the user in if it's valid.
+     */
+    private function attemptLoginFromCookie(): void
+    {
+        // Get the request service
+        $request = service('request');
+        
+        // Guard clause: If we are already logged in or there is no cookie, do nothing.
+        if (session()->get('is_logged_in') || ! $request->getCookie('remember_me')) {
+            return;
+        }
+
+        // Try to parse the cookie.
+        $cookieData = explode(':', $request->getCookie('remember_me'));
+        if (count($cookieData) !== 2) {
+            return;
+        }
+        [$selector, $validator] = $cookieData;
+        
+        // Look up the token by its selector in the database.
+        $tokenModel = new AuthTokenModel();
+        $token = $tokenModel->where('selector', $selector)->first();
+        
+        // If a token was found and the validator matches the hash in the DB...
+        if ($token && password_verify($validator, $token['hashed_validator'])) {
+            // Find the associated user.
+            $userModel = new UserModel();
+            $user = $userModel->find($token['user_id']);
+
+            if ($user) {
+                // SUCCESS! The token is valid.
+                // 1. Create a new session for the user.
+                $this->setUserSession($user); // Method from our AuthTrait
+                
+                // 2. (Security) Invalidate the old token and create a fresh one.
+                $tokenModel->delete($token['id']);
+                $this->createRememberMeToken($user['id']); // Method from our AuthTrait
+            }
+        }
     }
 }
